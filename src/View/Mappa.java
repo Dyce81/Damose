ee98bcddaef544ec72e73e055f23712cd0e90042
Dyce81 <dadocoppolino81@gmail.com>
2025-05-15 10:13:31 +0200

    CARICAMENTO PROGETTO

--- src/View/Mappa.java
+++ src/View/Mappa.java
@@ -1,5 +1,7 @@
 package View;
 
+import Controller.Wifi;
+
 import org.jxmapviewer.JXMapViewer;
 import org.jxmapviewer.OSMTileFactoryInfo;
 import org.jxmapviewer.input.PanKeyListener;
@@ -16,27 +18,35 @@ public class Mappa
     //costruttore
     Mappa(JFrame frame)
     {
-        //Definizione mappa - TODO: commentare tutto
-        JXMapViewer mappa = new JXMapViewer();
+        if (Wifi.wifi_connesso())
+        {
+            //Definizione mappa - TODO: commentare tutto
+            JXMapViewer mappa = new JXMapViewer();
 
-        TileFactoryInfo info = new OSMTileFactoryInfo();
-        DefaultTileFactory tileFactory = new DefaultTileFactory(info);
-        mappa.setTileFactory(tileFactory);
+            TileFactoryInfo info = new OSMTileFactoryInfo();
+            DefaultTileFactory tileFactory = new DefaultTileFactory(info);
+            mappa.setTileFactory(tileFactory);
 
-        tileFactory.setThreadPoolSize(8);
+            tileFactory.setThreadPoolSize(8);
 
-        //Metti Roma al centro della mappa
-        GeoPosition posizione_roma = new GeoPosition(41.90, 12.48);
-        mappa.setAddressLocation(posizione_roma);
-        mappa.setZoom(7);
+            //Metti Roma al centro della mappa
+            GeoPosition posizione_roma = new GeoPosition(41.90, 12.48);
+            mappa.setAddressLocation(posizione_roma);
+            mappa.setZoom(7);
 
-        frame.add(mappa, BorderLayout.CENTER);
+            frame.add(mappa, BorderLayout.CENTER);
 
-        MouseInputListener input_mouse = new PanMouseInputListener(mappa);
-        mappa.addMouseListener(input_mouse);
-        mappa.addMouseMotionListener(input_mouse);
-        mappa.addMouseWheelListener(new ZoomMouseWheelListenerCursor(mappa));
-        mappa.addMouseWheelListener(new ZoomMouseWheelListenerCursor(mappa));
-        mappa.addKeyListener(new PanKeyListener(mappa));
+            MouseInputListener input_mouse = new PanMouseInputListener(mappa);
+            mappa.addMouseListener(input_mouse);
+            mappa.addMouseMotionListener(input_mouse);
+            mappa.addMouseWheelListener(new ZoomMouseWheelListenerCursor(mappa));
+            mappa.addMouseWheelListener(new ZoomMouseWheelListenerCursor(mappa));
+            mappa.addKeyListener(new PanKeyListener(mappa));
+        }
+
+        else if (!Wifi.wifi_connesso())
+        {
+
+        }
     }
 }
