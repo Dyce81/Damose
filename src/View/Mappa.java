05eecbcea75e2313a899fe3b803053fc174d4f20
Andrea De Cupis <andreadec06@gmail.com>
2025-05-19 11:17:27 +0200

    Aggiornamento mappa - le icone scompaiono se lo zoom non è abbastanza elevato

--- src/View/Mappa.java
+++ src/View/Mappa.java
@@ -10,21 +10,23 @@ import org.jxmapviewer.input.ZoomMouseWheelListenerCursor;
 import org.jxmapviewer.viewer.*;
 import org.jxmapviewer.painter.Painter;
 
-import org.jxmapviewer.cache.FileBasedLocalCache;
-import java.io.File;
-
 import javax.swing.JFrame;
 import javax.swing.event.MouseInputListener;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 
 public class Mappa
 {
+    private int zoom = 3;
     public JXMapViewer mappa;
+    public Painter painter = null;
 
     //costruttore
     Mappa(JFrame frame)
     {
-            mappa = new JXMapViewer();
+        mappa = new JXMapViewer();
 
         if (Wifi.wifi_connesso())
         {
@@ -34,10 +36,9 @@ public class Mappa
 
             tileFactory.setThreadPoolSize(8);
         }
-
         else
         {
-            TileFactoryInfo info = new OSMTileFactoryInfo("ZIP archive", "jar:file:/C:/Users/Dado/IdeaProjects/Damose/data/.jxmapviewer2/tile.openstreetmap.zip!");
+            TileFactoryInfo info = new OSMTileFactoryInfo("ZIP archive", "jar:file:/././data/.jxmapviewer2/tile.openstreetmap.zip!");
             TileFactory tileFactory = new DefaultTileFactory(info);
             mappa.setTileFactory(tileFactory);
         }
@@ -45,20 +46,36 @@ public class Mappa
         //Metti Roma al centro della mappa
         GeoPosition posizione_roma = new GeoPosition(41.90, 12.48);
         mappa.setAddressLocation(posizione_roma);
-        mappa.setZoom(7);
+        mappa.setZoom(3);
 
         frame.add(mappa, BorderLayout.CENTER);
 
         MouseInputListener input_mouse = new PanMouseInputListener(mappa);
         mappa.addMouseListener(input_mouse);
         mappa.addMouseMotionListener(input_mouse);
-        mappa.addMouseWheelListener(new ZoomMouseWheelListenerCursor(mappa));
-        mappa.addMouseWheelListener(new ZoomMouseWheelListenerCursor(mappa));
+        mappa.addMouseWheelListener(new ZoomMouseWheelListenerCursor(mappa)
+        {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e)
+            {
+                // non ottimale
+                int dir = e.getWheelRotation();
+                zoom += 1 * dir;
+                mappa.setZoom(zoom);
+                if (zoom < 5) {
+                    set_painter(painter);
+                } else {
+                    set_painter(null);
+                }
+
+            }
+        });
         mappa.addKeyListener(new PanKeyListener(mappa));
     }
 
     public void set_painter(Painter p)
     {
+        if (painter == null) painter = p; // se painter non è già stato definito, impostalo correttamente
         mappa.setOverlayPainter(p);
     }
 }
