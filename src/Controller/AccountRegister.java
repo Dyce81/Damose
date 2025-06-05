7988b92ab2f22b1e30b1ac821942b3f399cd0cff
Dado <dadocoppolino81@gmail.com>
2025-06-05 16:27:54 +0200

    ho fatto in modo che la pagina di login reagisca quando password e utente non rientrano negli standard

--- src/Controller/AccountRegister.java
+++ src/Controller/AccountRegister.java
@@ -1,7 +1,37 @@
 package Controller;
+import java.util.Vector;
 
 public class AccountRegister
 {
-    String[] usernames;
-    String[] passwords;
+    //Da sistemare: username e password non vengono memorizzati se si chiude l'applicazione
+    static final Vector<String> usernames = new Vector<>();
+    static final Vector<String> passwords = new Vector<>();
+
+    //0 = user troppo corto, 1 user troppo lungo, 2 pswd troppo corta, 3 pswd troppo lunga, 4 user già esistente, 5 nessun problema
+    public static int registra_utente(String username, String password)
+    {
+        if (username.length() < 5) {return 0;}
+        else if (username.length() > 15) {return 1;}
+        else if (password.length() < 6) {return 2;}
+        else if (password.length() > 12) {return 3;}
+        for (String i : usernames)
+        {
+            if (username.equals(i)) {return 4;}
+        }
+        usernames.add(username);
+        passwords.add(password);
+        return 5;
+    }
+
+    public static void mostra_credenziali()
+    {
+        System.out.println(usernames);
+        System.out.println(passwords);
+    }
+
+    public static void elimina_credenziali()
+    {
+        usernames.clear();
+        passwords.clear();
+    }
 }
--- src/View/LoginPage.java
+++ src/View/LoginPage.java
@@ -1,5 +1,7 @@
 package View;
 
+import Controller.AccountRegister;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -11,26 +13,29 @@ public class LoginPage
     {
         //setup finestra
         JDialog loginPage = new JDialog();
-        loginPage.setSize(450,270);
+        loginPage.setSize(450,280);
         loginPage.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
         loginPage.setLocationRelativeTo(null);
         loginPage.setModal(true);
         loginPage.setLayout(null);
 
+
         //pannello in cui inserire i componenti
         JPanel panel = new JPanel();
-        panel.setBounds(68, 20, 300, 180);
+        panel.setBounds(68, 20, 300, 210);
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         loginPage.add(panel);
 
+
         JLabel benvenuto = new JLabel("benvenuto");
-        benvenuto.setText("Benvenuto nella pagina di login");
+        benvenuto.setText("BENVENUTO NELLA PAGINA DI LOGIN");
         benvenuto.setAlignmentX(Component.CENTER_ALIGNMENT);
         panel.add(benvenuto);
         panel.add(Box.createVerticalStrut(10));
 
-        JTextField username = new JTextField(3);
-        String username_input = username.getText();
+
+        JTextField username = new JTextField("usare dai 5 ai 15 caratteri",3);
+        username.setForeground(Color.LIGHT_GRAY);
         username.setAlignmentX(Component.CENTER_ALIGNMENT);
         JLabel usrn_txt = new JLabel("username:");
         usrn_txt.setLabelFor(username);
@@ -39,8 +44,10 @@ public class LoginPage
         panel.add(username);
         panel.add(Box.createVerticalStrut(10));
 
-        JTextField password = new JTextField(3);
-        String password_input = password.getText();
+
+        //si potrebbe usare JPasswordField invece di JTextField
+        JTextField password = new JTextField("usare dai 6 ai 12 caratteri", 3);
+        password.setForeground(Color.LIGHT_GRAY);
         password.setAlignmentX(Component.CENTER_ALIGNMENT);
         JLabel pswd_txt = new JLabel("password:");
         pswd_txt.setLabelFor(password);
@@ -49,35 +56,87 @@ public class LoginPage
         panel.add(password);
         panel.add(Box.createVerticalStrut(10));
 
-        JButton accedi = new JButton("accedi");
-        accedi.setText("Accedi");
-        accedi.setAlignmentX(Component.CENTER_ALIGNMENT);
-        panel.add(accedi);
-        panel.add(Box.createVerticalStrut(10));
 
-        JButton registrati = new JButton("registrati");
-        registrati.setText("Non sei registrato? Registrati ora!");
-        registrati.setAlignmentX(Component.CENTER_ALIGNMENT);
-        panel.add(registrati);
-        registrati.addActionListener(new ActionListener()
+        JButton acc_reg = new JButton("acc_reg");
+        acc_reg.setText("Accedi");
+        acc_reg.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(acc_reg);
+        panel.add(Box.createVerticalStrut(10));
+        JLabel avviso = new JLabel("testo deafault");
+        avviso.setForeground(loginPage.getBackground());
+        avviso.setAlignmentX(Component.CENTER_ALIGNMENT);
+        acc_reg.addActionListener(new ActionListener()
+        {public void actionPerformed(ActionEvent e)
+            {
+                String username_input = username.getText();
+                String password_input = password.getText();
+                //AccountRegister.elimina_credenziali();
+                if (acc_reg.getText().equals("Registrati"))
+                {
+                    if (AccountRegister.registra_utente(username_input, password_input) == 5)
+                    {
+                        avviso.setText("Registrazione completata! Accesso eseguito!");
+                        avviso.setForeground(Color.GREEN);
+                    }
+                    else if (AccountRegister.registra_utente(username_input, password_input) == 0)
+                    {
+                        avviso.setText("username troppo corto!");
+                        avviso.setForeground(Color.RED);
+                    }
+                    else if (AccountRegister.registra_utente(username_input, password_input) == 1)
+                    {
+                        avviso.setText("username troppo lungo!");
+                        avviso.setForeground(Color.RED);
+                    }
+                    else if (AccountRegister.registra_utente(username_input, password_input) == 2)
+                    {
+                        avviso.setText("password troppo corta!");
+                        avviso.setForeground(Color.RED);
+                    }
+                    else if (AccountRegister.registra_utente(username_input, password_input) == 3)
+                    {
+                        avviso.setText("password troppo lunga!");
+                        avviso.setForeground(Color.RED);
+                    }
+                    else if (AccountRegister.registra_utente(username_input, password_input) == 4)
+                    {
+                        avviso.setText("username già esistente!");
+                        avviso.setForeground(Color.RED);
+                    }
+                    //AccountRegister.mostra_credenziali();
+                }
+            }
+        });
+
+
+        JButton cambia_mod = new JButton("cambia_modalità");
+        cambia_mod.setText("Non sei registrato? Registrati ora!");
+        cambia_mod.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(cambia_mod);
+        panel.add(Box.createVerticalStrut(10));
+        cambia_mod.addActionListener(new ActionListener()
         {public void actionPerformed(ActionEvent e)
         {
 
-            if (accedi.getText().equals("Accedi"))
+            if (acc_reg.getText().equals("Accedi"))
+
             {
-                registrati.setText("Hai già un account? Accedi ora!");
-                accedi.setText("Registrati");
-                benvenuto.setText("Benvenuto nella pagina di registrazione");
+                cambia_mod.setText("Hai già un account? Accedi ora!");
+                acc_reg.setText("Registrati");
+                benvenuto.setText("BENVENUTO NELLA PAGINA DI REGISTRAZIONE");
             }
-            else if (accedi.getText().equals("Registrati"))
+            else if (acc_reg.getText().equals("Registrati"))
             {
-                registrati.setText("Non sei registrato? Registrati ora!");
-                accedi.setText("Accedi");
-                benvenuto.setText("Benvenuto nella pagina di login");
+                cambia_mod.setText("Non sei registrato? Registrati ora!");
+                acc_reg.setText("Accedi");
+                benvenuto.setText("BENVENUTO NELLA PAGINA DI LOGIN");
             }
 
         }
         });
+
+
+        panel.add(avviso);
 
         loginPage.add(panel);
         loginPage.setVisible(true);
