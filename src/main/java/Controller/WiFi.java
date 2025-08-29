98a646acf906c1f79685be5230be320689fca63f
Andrea De Cupis <andreadec06@gmail.com>
2025-08-29 18:47:49 +0200

    Refactoring della classe della Mappa

--- src/main/java/Controller/WiFi.java
+++ src/main/java/Controller/WiFi.java
@@ -1,60 +1,55 @@
 package Controller;
 
-import java.net.InetAddress;
+import View.Frame;
+import View.Mappa;
+
+import java.net.*;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class WiFi
 {
-    public static boolean WiFi = false;
-    private static View.Frame riferimentoFrame;
-    private static View.Mappa riferimentoMappa;
+    private static Frame riferimentoFrame;
+    private static Mappa riferimentoMappa;
 
-    public static void impostaFrame(View.Frame frame, View.Mappa mappa)
+    public static void impostaFrame(Frame frame, Mappa mappa)
     {
         riferimentoFrame = frame;
         riferimentoMappa = mappa;
     }
 
-    public static void wifi_controller()
+    public static void inizializza()
     {
         //questa cosa non mi convice al 100% - probabilmente da rivedere
-        Timer timer_controllo_wifi = new Timer();
-        TimerTask task_controllo = new TimerTask()
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask()
         {
             @Override
             public void run()
             {
-                //TODO: fare cose effettivamente
-                if (wifi_connesso())
+                if (riferimentoFrame != null)
                 {
-                    WiFi = true;
-                    if (riferimentoFrame != null)
-                    {
-                        riferimentoFrame.cambiaStatoWiFi();
-                        riferimentoMappa.cambiaStatoMappa();
-                    }
-                }
-                else if (!wifi_connesso()) {
-                    WiFi = false;
-                    if (riferimentoFrame != null)
-                    {
-                        riferimentoFrame.cambiaStatoWiFi();
-                        riferimentoMappa.cambiaStatoMappa();
-                    }
+                    riferimentoFrame.cambiaStatoWiFi();
+                    riferimentoMappa.cambiaStatoMappa();
                 }
             }
         };
 
-        timer_controllo_wifi.scheduleAtFixedRate(task_controllo, 0, 10000);
+        timer.scheduleAtFixedRate(task, 0, 10000);
         //TODO: il "period" reale dovrebbe essere 30000
     }
 
-    public static boolean wifi_connesso()
+    public static boolean connesso()
     {
-        try {
-            InetAddress net = InetAddress.getByName("google.com");
-        } catch (Exception e) {
+        try
+        {
+            URL url = new URI("https://www.google.com/").toURL();
+            URLConnection conn = url.openConnection();
+            conn.setConnectTimeout(5000); //Non so se sono necessari? (forse meglio averli comunque?)
+            conn.setReadTimeout(3000);
+        }
+        catch (Exception e)
+        {
             return false;
         }
 
