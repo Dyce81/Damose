d68064a4d97f310b8b62f04ca18649e98ca3acdf
Andrea De Cupis <andreadec06@gmail.com>
2025-09-12 16:27:57 +0200

    Test inutile

--- src/test/java/StaticGTFSTest.java
+++ src/test/java/StaticGTFSTest.java
@@ -1,22 +1,11 @@
 import Controller.StaticGTFS;
-import org.junit.jupiter.api.Assertions;
+import View.LoadingScreen;
 import org.junit.jupiter.api.Test;
-
-//import org.junit.Test;
-
-//import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StaticGTFSTest
 {
     // Le linee delle metro sono 248, 249, 305, 342
-
-    @Test
-    void lineaDellaMetroNonDeveEsistere()
-    {
-        StaticGTFS.inizializzaDati();
-        assertFalse(StaticGTFS.lineaDellaMetro("0"));
-    }
 
     @Test
     void lineaDellaMetroCorretta()
